f4b94e684ae6ff09cc8d7382de1059423db1e433
sirmanners2 <jonas.cederberg@gmail.com>
2015-09-17 11:42:08 +0200

    lektion, jobbar kap 6

--- Kap 6.py
+++ Kap 6.py
@@ -1,17 +1,28 @@
 
-"""
+'''
 for i in range(0, 10):
     for j in range(10-i):
         print(j, end = " ")
     print()
-"""
 
-for row in range(0, 10):
-    for space in range(0, row):
-        print(end = "  ")
-    for column in range(0, 10-row):
+for row in range(10):
+    for space in range(row):
+        print(" ", end = " ")
+    for column in range(10-row):
         print(column, end = " ")
     print()
 
+for row in range (9):
+    for column in range(9):
+        if (row+1)*(column+1)<10:
+            print(end = " ")
+        print((row+1)*(column+1), end = " " )
+    print()
 
+'''
 
+for row in range(9):
+    for column in range(row+1):
+        print(column+1, end=" ")
+
+    print()
