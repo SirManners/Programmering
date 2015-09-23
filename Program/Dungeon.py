c769bcf6849c37b96c3139d9a7336604d61d31dd
sirmanners2 <jonas.cederberg@gmail.com>
2015-09-23 12:03:00 +0200

    lektion, jobbar kap 6

---
+++ Program/Dungeon.py
@@ -0,0 +1,10 @@
+__author__ = 'ab53995'
+
+room_list = []
+room = ["You wake up. Where do you go?" , 1, 2, 3, 4]
+room_list.append(room)
+
+
+current_room = 0
+
+print(room_list)
--- Program/Quiz.py
+++ Program/Quiz.py
@@ -9,50 +9,63 @@
 # Licence:     <your licence>
 # Import the math library
 
-#-------------------------------------------------------------------------------
-# förändring
-from math import *
+# -------------------------------------------------------------------------------
+# fÃ¶rÃ¤ndring
 
 print("Welcome")
 print()
 score = 0
 questions = 0
-answer_0 = input("Hello. Do you want to play a game?")
+answer_0 = input("Hello. Do you want to play a quiz?")
 if answer_0.lower() == "yes":
     print("Excellent")
 else:
     print("This is not a democracy")
 print()
-questions = questions + 1
-answer_1 = int(input("Question one: How much is 2^64?"))
-if answer_1 == 18446744073709551616:
+questions += 1
+answer_1 = input("Question one: How much is 2^64? A) 16 B) 18446744073709551166 C) 18446744073709551616")
+if answer_1.lower() == "c":
     print("Congratulations, you know how to use a calculator")
-    score = score + 1
+    score += 1
 else:
     print("You are not doing too well are you")
 print()
 
-questions = questions + 1
+questions += 1
 answer_2 = input("Question two: What are my middle names?")
 if answer_2.lower() == "martin joel":
     print("Correct! You know me too well")
-    score = score + 1
+    score += 1
+elif answer_2.lower() == "martin" or "joel":
+    print("Almost, half a point.")
+    score += 0.5
 else:
     print("You should have come prepared")
 print()
 
-questions = questions + 1
-answer_3 = input("This is the last question")
+questions += 1
+answer_3 = input("Last question: Is the last question?")
 if answer_3.lower() == "yes":
     print("Clever boy")
-    score = score + 1
+    score += 1
 else:
     print("You suck balls")
-    score = score - 500
+    score -= 500
 
 print()
 
 score_procent = score / questions * 100
 print("You have scored" , score , "points," , score_procent , "% of the total.")
 
+if score_procent == 100:
+    print("You are winner!")
+elif score_procent > 70:
+    print("Well done, but not good enough.")
+elif score_procent > 20 and score_procent < 70:
+    print("Mediocre.")
+elif score_procent < 20:
+    print("Did you even try?")
+
+elif score_procent < 0:
+    print("You are awful")
 
