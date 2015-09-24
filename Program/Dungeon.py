a5c42fb8c9a2e7547737658d7fce6d482a934a7d
ab53995 <jonas.cederberg@gmail.com>
2015-09-24 14:48:59 +0200

    Dungeon fortsättning

--- Program/Dungeon.py
+++ Program/Dungeon.py
@@ -2,11 +2,16 @@ __author__ = 'ab53995'
 
 # 1 - Norr 2 - Öster 3 - Söder 4 - Väster
 
-def key():
-
-
-
 inventory_list = []
+empty = [None, None]
+inventory_list.append(empty)
+
+# def lock():
+#    if inventory_list[0] == "Key":
+#        return 3
+#    return 2
+
+
 item = ["Key"]
 
 room_list = []
@@ -16,14 +21,11 @@ room_list.append(room)
 room = ["The light came from a nearly burned out torch. You can see the line of torches continuing further down in the dungeon", 2, None, 0, None, None]
 room_list.append(room)
 
-room = ["The torches lead up to a great iron door.", 3, None, 1, None, None]
+room = ["The torches lead up to a great iron door.", 2, None, 1, None, None]
 room_list.append(room)
 
 room = ["Beyond the door lies endless treasures. Your adventure has been very fruitful and you retire to a castle by the sea.", None, None, None, None]
 room_list.append(room)
-
-
-
 
 
 current_room = 0
@@ -76,12 +78,21 @@ while not done:
         if item_take == None:
             print("There is nothing to take here")
         inventory_list.append(room_list[current_room][5])
+        room_list[2][1] = 3
+
+    elif answer == "look":
+        if current_room[5] != None:
+            print("There is a ", current_room[5], "on the ground")
+        # current room är en siffra i en lista
+        else:
+            print("There's nothing here")
 
     if answer == "help":
         print("\nYou can go in 4 different directions: North, East, South and West")
         print("If you want to pick up something write take")
         print("To quit press q")
         print("To check inventory type inventory")
+        print("Type look to see what's in the room.")
 
 
 
