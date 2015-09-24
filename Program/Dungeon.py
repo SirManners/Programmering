b47aeff07c24a5b0ca5dabd3938820c5cf1a5dd3
Yudhistira <jonas.cederberg@gmail.com>
2015-09-24 16:25:38 +0200

    fixade lite med bild

--- Program/Dungeon.py
+++ Program/Dungeon.py
@@ -3,8 +3,7 @@ __author__ = 'ab53995'
 # 1 - Norr 2 - Öster 3 - Söder 4 - Väster
 
 inventory_list = []
-empty = [None, None]
-inventory_list.append(empty)
+
 
 # def lock():
 #    if inventory_list[0] == "Key":
@@ -15,7 +14,7 @@ inventory_list.append(empty)
 item = ["Key"]
 
 room_list = []
-room = ["You wake up in a dark dungeon. You see the light of flickering torches in the north. There is a key on the ground" , 1, None, None, None, item]
+room = ["\nYou wake up in a dark dungeon. You see the light of flickering torches in the north." , 1, None, None, None, item]
 room_list.append(room)
 
 room = ["The light came from a nearly burned out torch. You can see the line of torches continuing further down in the dungeon", 2, None, 0, None, None]
@@ -24,7 +23,7 @@ room_list.append(room)
 room = ["The torches lead up to a great iron door.", 2, None, 1, None, None]
 room_list.append(room)
 
-room = ["Beyond the door lies endless treasures. Your adventure has been very fruitful and you retire to a castle by the sea.", None, None, None, None]
+room = ["You unlocked the iron door with your key. \n Beyond the door lies endless treasures. Your adventure has been very fruitful and you retire to a castle by the sea.", None, None, None, None]
 room_list.append(room)
 
 
@@ -44,45 +43,51 @@ while not done:
 
     elif answer == "north":
         next_room = room_list[current_room][1]
-        if next_room == None:
+        if next_room is None:
             print("You can't go that way")
             continue
         current_room = next_room
 
     elif answer == "east":
         next_room = room_list[current_room][2]
-        if next_room == None:
+        if next_room is None:
             print("You can't go that way")
             continue
         current_room = next_room
 
     elif answer == "south":
         next_room = room_list[current_room][3]
-        if next_room == None:
+        if next_room is None:
             print("You can't go that way")
             continue
         current_room = next_room
 
     elif answer == "west":
         next_room = room_list[current_room][4]
-        if next_room == None:
+        if next_room is None:
             print("You can't go that way")
             continue
         current_room = next_room
 
     elif answer == "inventory":
-        print(inventory_list[0][0])
+        if inventory_list is None:
+            print("")
+        print(inventory_list, )
 
     elif answer == "take":
         item_take = room_list[current_room][5]
-        if item_take == None:
+        if item_take is None:
             print("There is nothing to take here")
+            continue
         inventory_list.append(room_list[current_room][5])
+        print("You picked up a", room_list[current_room][5])
         room_list[2][1] = 3
+        room_list[current_room][5] = None
+
 
     elif answer == "look":
-        if current_room[5] != None:
-            print("There is a ", current_room[5], "on the ground")
+        if room_list[current_room][5] is not None:
+            print("There is a", room_list[current_room][5], "on the ground")
         # current room är en siffra i en lista
         else:
             print("There's nothing here")
