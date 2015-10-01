b2d3b0fe20fc19ea5b8221c8e8afeab9fae664cf
ab53995 <jonas.cederberg@gmail.com>
2015-10-01 15:53:48 +0200

    Kap 8 o 9

--- Kap 8.py
+++ Kap 8.py
@@ -1,25 +1,24 @@
 __author__ = 'ab53995'
 
-from math import *
 import random
 import pygame
 
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-GREEN    = (   0, 255,   0)
-RED      = ( 255,   0,   0)
-BROWN    = ( 77,   18,  18)
-YELLOW   = ( 255, 251,   0)
-BLUE     = (   0,   4, 255)
-NIGHTBLUE =(  0,   1,   64)
-STARBLUE = (159, 161, 252)
-GREY = (50, 50, 82)
+BLACK     = (   0,   0,   0)
+WHITE     = ( 255, 255, 255)
+GREEN     = (   0, 255,   0)
+RED       = ( 255,   0,   0)
+BROWN     = (  77,  18,  18)
+YELLOW    = ( 255, 251,   0)
+BLUE      = (   0,   4, 255)
+NIGHTBLUE = (   0,   1,  64)
+STARBLUE  = ( 159, 161, 252)
+GREY      = (  50,  50,  82)
 
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_mode((1366,768),pygame.FULLSCREEN)
-
 """
+
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Kapitel 5")
@@ -28,25 +27,101 @@ pygame.display.set_caption("Kapitel 5")
 snow_list = []
 for i in range(50):
     x = random.randrange(0, 1366)
-    y = random.randrange(0, 768)
+    y = random.randrange(-768, 0)
     snow_list.append([x, y])
 
 done = False
 clock = pygame.time.Clock()
-rekt_x = 50
-rekt_y = 50
-rekt_change_x = 5
-rekt_change_y = 5
-y_mouse =
-x_mouse =
+
+rekt_x1 = 0
+rekt_y1 = 300
+
+rekt_x2 = 50
+rekt_y2 = 50
+
+rekt_change_x1 = 10
+rekt_change_y1 = 30
+
+rekt_change_x2 = 5
+rekt_change_y2 = 5
+
+x_mouse = 0
+y_mouse = 0
+
+x_speed = 0
+y_speed = 0
+
+x_coord = 10
+y_coord = 600
+
+time1 = 0
+time2 = 0
+time_down = pygame.time.get_ticks()
+acc = 0
+keydown = [None, False, False, False, False]
+
+
 while not done:
+    if keydown[1] or keydown[2] or keydown[3] or keydown[4]:
+        time2 = pygame.time.get_ticks() - time1
+        print(time2/1000)
+        acc = 0.5 * time2/1000
+        if keydown[1]:
+            y_speed -= acc
+        if keydown[2]:
+            y_speed += acc
+        if keydown[3]:
+            x_speed -= acc
+        if keydown[4]:
+            x_speed += acc
+
     for event in pygame.event.get():
-        # Gör att programet stängs när man trycker på en knapp
+
         if event.type == pygame.KEYDOWN:
-            done = True
-        if event.type == pygame.QUIT:
-            print("User has asked to quit.")
-            done = True
+
+            time1 = pygame.time.get_ticks()
+            if event.key == pygame.K_w:
+                y_speed = -5
+                keydown[1] = True
+            if event.key == pygame.K_s:
+                y_speed = 5
+                keydown[2] = True
+            if event.key == pygame.K_a:
+                x_speed = -5
+                keydown[3] = True
+            if event.key == pygame.K_d:
+                x_speed = 5
+                keydown[4] = True
+            if event.key == pygame.K_ESCAPE:
+                done = True
+
+        if event.type == pygame.KEYUP:
+
+            if event.key == pygame.K_w:
+                keydown[1] = False
+                if keydown[2] == False and keydown[3] == False and keydown[4]== False:
+                    y_speed =  0
+                    x_speed = 0
+
+            if event.key == pygame.K_s:
+                keydown[2] = False
+                if keydown[1] == False and keydown[3] == False and keydown[4]== False :
+                    y_speed =  0
+                    x_speed = 0
+
+            if event.key == pygame.K_a:
+                keydown[3] = False
+                if keydown[1] == False and keydown[2] == False and keydown[4]== False :
+                    x_speed =  0
+                    y_speed = 0
+            if event.key == pygame.K_d:
+                keydown[4] = False
+                if keydown[2] == False and keydown[3] == False and keydown[1]== False :
+                    x_speed =  0
+                    y_speed = 0
+
+            if event.key == pygame.K_ESCAPE:
+                done = True
 
         if event.type == pygame.QUIT:
             print("User has asked to quit.")
@@ -54,22 +129,42 @@ while not done:
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
 
-    pos = pygame.mouse.get_pos()
-    x = pos[0]
-    y = pos[1]
+            pos = pygame.mouse.get_pos()
+            x_mouse = pos[0]
+            y_mouse = pos[1]
+
+    x_coord += x_speed
+    y_coord += y_speed
+
+    # lägg till något som dödar dig
+
 
     screen.fill(NIGHTBLUE)
 
-    """pygame.draw.rect(screen, WHITE, [rekt_x, rekt_y, 50, 50])
-    pygame.draw.rect(screen, RED, [rekt_x + 10, rekt_y + 10, 30, 30])
-    rekt_x += rekt_change_x
-    rekt_y += rekt_change_y
-    if rekt_x > 1316 or rekt_x < 0:
-        rekt_change_x *= -1
-    if rekt_y >718 or rekt_y < 0:
-        rekt_change_y *= -1"""
+    pygame.draw.rect(screen, WHITE, [rekt_x1, rekt_y1, 50, 50])
+    pygame.draw.rect(screen, RED, [rekt_x1 + 10, rekt_y1 + 10, 30, 30])
+    rekt_x1 += rekt_change_x1
+    rekt_y1 += rekt_change_y1
+    if rekt_x1 > 1316 or rekt_x1 < 0:
+        rekt_change_x1 *= -1
+    if rekt_y1 >718 or rekt_y1 < 0:
+        rekt_change_y1 *= -1
 
-    """for i in range(len(snow_list)):
+    pygame.draw.rect(screen, WHITE, [rekt_x2, rekt_y2, 50, 50])
+    pygame.draw.rect(screen, RED, [rekt_x2 + 10, rekt_y2 + 10, 30, 30])
+    rekt_x2 += rekt_change_x2
+    rekt_y2 += rekt_change_y2
+    if rekt_x2 > 1316 or rekt_x2 < 0:
+        rekt_change_x2 *= -1
+    if rekt_y2 >718 or rekt_y2 < 0:
+        rekt_change_y2 *= -1
+
+
+
+
+    pygame.draw.rect(screen, RED, [x_coord, y_coord, 15, 15])
+
+    for i in range(len(snow_list)):
         pygame.draw.circle(screen, WHITE, snow_list[i], 2)
         snow_list[i][1] += 1
         if snow_list[i][1] > 768:
@@ -78,15 +173,10 @@ while not done:
             x = random.randrange(0, 1366)
             snow_list[i][0] = x
 
-"""
 
-    text_2 = "x=", x_mouse, "\ny=", y_mouse
-    font = pygame.font.SysFont('Courier New', 300, True, False)
-    text = font.render(text_2, True, BLACK)
-    screen.blit(text, [x_mouse, y_mouse])
 
     pygame.display.flip()
 
-    clock.tick(60)
+    clock.tick(120)
 
 pygame.quit()
