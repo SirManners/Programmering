0313e4eaa4e0a1f491678a95908291113958ed20
ab53995 <jonas.cederberg@gmail.com>
2015-10-19 14:07:26 +0200

    Eulerproblem

--- Eulerproblem 3.py
+++ Eulerproblem 3.py
@@ -1,23 +1,18 @@
+import math
+
 __author__ = 'ab53995'
 
 
 def f():
-    factors = []
-    x = 3
-    y = 0
+    x = 600851475142
     z = 1
-    Tal = 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-    while x < 200283825047:
+    while x > 2:
         # while x > z:
         # x % Tal != 0
         # prime = x % z > 0
         if 600851475143 % x == 0:
             # and x == prime
-            number = x
-            factors.append(number)
             print(x)
-            y += 1
-        x += 2
-    print(factors[y])
 
+        x -= 1
 f()
