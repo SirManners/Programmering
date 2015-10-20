aa8a6a95dc827db88845cc7fb34a35d2338056b8
ab53995 <jonas.cederberg@gmail.com>
2015-10-20 22:04:46 +0200

    Eulerproblem

--- Eulerproblem 3.py
+++ Eulerproblem 3.py
@@ -1,18 +1,24 @@
 import math
 
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
 __author__ = 'ab53995'
 
 
 def f():
-    x = 600851475142
+    x = 1
     z = 1
-    while x > 2:
+    while x < 600851475143:
         # while x > z:
         # x % Tal != 0
         # prime = x % z > 0
         if 600851475143 % x == 0:
             # and x == prime
-            print(x)
+            z *= x
+            if z == 600851475143:
+                print(x)
+                break
+        x += 2
 
-        x -= 1
 f()
---
+++ Eulerproblem 4.py
@@ -0,0 +1,31 @@
+__author__ = 'ab53995'
+
+# A palindromic number reads the same both ways.
+
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+def f():
+    palindrome = []
+    x = 999
+    y = 100
+    z = 0
+    i = 1
+    while x >= 100:
+        z = x * y
+        palindrome.append(z)
+        if palindrome[] == :
+        x -= 1
+        y += 1
+
+
+def g():
+
+
+
+x = []
+x.append("200")
+
+print(x[0][1])
