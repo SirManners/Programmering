3cd8484bfd93c95b5a1448a9d23ee1d00c9fb703
ab53995 <jonas.cederberg@gmail.com>
2015-12-10 11:39:05 +0100

    game klassen, få den spelbar

--- Projekt/Spel.py
+++ Projekt/Spel.py
@@ -3,6 +3,8 @@ import random
 import Klasser.färger
 
 Klasser.färger()
+SCREEN_HEIGHT = 688
+SCREEN_WIDTH = 1366
 
 class Spelare(pygame.sprite.Sprite):
     def __init__(self):
@@ -20,28 +22,73 @@ class Fiendermall(pygame.sprite.Sprite):
         self.image = pygame.Surface([20, 20])
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.move_x = 1
-        self.move_y = 1
+        self.move_x = 0
+        self.move_y = 0
         # self.remove_width
         # self.remove_height
     def reset_pos(self):
-
+        self.rect.y = random.randrange(300, 500)
+        self.rect.x = random.randrange(SCREEN_WIDTH)
     def update(self):
         self.rect.y += self.move_y
         self.rect.x += self.move_x
-        if 0 + self.rect.height > self.rect.x > SCREEN_WIDH + self.rect.height:
+        # if 0 + self.rect.height > self.rect.x > SCREEN_WIDTH + self.rect.height:
+        # ta bort
+        # if 0 + self.rect.width > self.rect.y > SCREEN_HEIGHT + self.rect.height:
+        # ta bort
 
-        if 0 + self.rect.width > self.rect.y > SCREEN_HEIGHT + self.rect.height:
+class Projektil(pygame.sprite.Sprite):
 
 class Game(object):
 
     def __init__(self):
 
+        self.score = 0
         self.game_over = False
 
+        # Create sprites lists
+        self.enemy_list = pygame.sprite.Group()
+        self.all_sprites_list = pygame.sprite.Group()
+
+        self.projectile_list = pygame.sprite.Group()
+        self.all_sprites_list.add(self.projectile_list)
+
+        self.player = Spelare()
+        self.all_sprites_list.add(self.player)
+
+        # Create the sprites
+        # Projektiler, skapa ifall variabel?
+        boss = Fiendermall()
+
     def process_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return True
+                if event.key == pygame.K_SPACE:
+                    if self.game_over:
+                        self.__init__()
 
     def run_logic(self):
+        if not self.game_over:
+            self.all_sprites_list.update()
+            projectile_hit_list = pygame.sprite.spritecollide(self.projectile, self.enemy_list, True)
+            enemy_hit_list = pygame.sprite.spritecollide(self.player, self.enemy.list, True)
+
+            for hit in enemy_hit_list:
+                self.game_over = True
+            for enemy in projectile_hit_list:
+                self.score += 1
 
     def display_frame(self, screen):
+        screen.fill(NIGHTBLUE)
 
+        if self.game_over:
+            """ rip meddelande """
+
+        if  not self.game_over:
+            self.all.sprites.list.draw(screen)
+
+        pygame.display.flip
--- Projekt/main.py
+++ Projekt/main.py
@@ -26,43 +26,31 @@ def main():
     clock = pygame.time.Clock()
     resume = False
     # spel = Spel.Game()
-    # game = Spel.Game()
+    game = Spel.Game()
 
     # Main loop
     Intro.öppna_intro(screen)
     while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    done_game = True
-                    # resume = True
-                    # gör så att man får upp en resumeknapp
-                if event.type == pygame.QUIT:
-                    print("User has asked to quit.")
-                    done_game = True
+        done = Meny.öppna_meny(screen, resume)
+        if not done:
+            done_game = False
 
-            screen.fill(NIGHTBLUE)
-            pygame.draw.rect(screen, WHITE, [100, 100, 50, 200])
+        while not done_game:
 
-            pygame.display.flip()
+            done_game = game.process_events()
+
+            game.run_logic()
+
+            game.display_frame(screen)
 
             clock.tick(60)
+    pygame.quit()
 
-        pygame.quit()
 
 if __name__ == "__main__":
     main()
 
 """
-        done = Meny.öppna_meny(screen, resume)
-        if not done:
-            done_game = False
-        while not done_game:
-            done_game = game.process_events()
-            game.run_logic()
-            game.display_frame(screen)
-            clock.tick(60)
-    pygame.quit()
 
 
 if __name__ == "__main__":
